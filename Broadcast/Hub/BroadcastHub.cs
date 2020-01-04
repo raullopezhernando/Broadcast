09759ab2b88331426109df73be60aa0ce54fd36d
RaulLH <raullopezhernando@gmail.com>
2020-01-04 17:54:49 +0100

    Agregar archivos de proyecto.

---
+++ Broadcast/Hub/BroadcastHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Broadcast.Hub
+{
+    // LADO DEL SERVIDOR
+
+    public class BroadcastHub : Microsoft.AspNet.SignalR.Hub
+    {
+        // Cada vez que los clientes que invoquen este metodo , internamente
+        // se llamara al metodo "displaytext· de los clientes para que reciban la informacion.
+        // Ahora hay que crear el metodo del lado del cliente
+        public void BroadcastMessage(string message)
+        {
+            Clients.All.displayText(message);
+        }
+    }
+}
---
+++ Broadcast/Starup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Broadcast.Starup))]
+
+// LADO DEL SERVIDOR
+
+namespace Broadcast
+{
+    public class Starup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR(); // Por defecto tomara la ruta "/signalr" aunque no se pongo como parametro
+        }
+    }
+}
